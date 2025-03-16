73a9874fc6a8751b1a6c8f21d54e304a8a4f4a16
CelestialInsomniac <annina.pfister@stud.hslu.ch>
2025-03-16 19:52:22 +0100

    included moons in code, fixed sources

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("pages");
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
     dir: {
       includes: "_includes",
+      input: "src",
       output: "_site"
     }
   };
