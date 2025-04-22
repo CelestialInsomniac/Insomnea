7b756e1f3ba1167695722a8deea9da52c185d7a9
CelestialInsomniac <annina.pfister@stud.hslu.ch>
2025-04-22 21:21:13 +0200

    Deleted moons, sites for fine art, added about
    updated navigation

--- src/_data/masonry.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.masonry-item').forEach(item => {
-    const img = item.querySelector('img');
-    if (img) {
-        const height = img.getBoundingClientRect().height;
-        item.style.setProperty('--item-height', height);
-    }
-});
