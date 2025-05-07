32b24549743d60e7fd06fd24dc78f503b407eb80
CelestialInsomniac <annina.pfister@stud.hslu.ch>
2025-05-07 21:32:18 +0200

    Fixed Navigation

--- .eleventy.js
+++ .eleventy.js
@@ -3,26 +3,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("pages");
   eleventyConfig.addPassthroughCopy("src/assets");
 
-  // Deutsche Concept-Art-Seiten
-  eleventyConfig.addCollection("conceptArtDE", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("src/pages/concept-art/**/*.njk");
-  });
-
-  // Englische Concept-Art-Seiten
-  eleventyConfig.addCollection("conceptArtEN", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("src/pages/en/concept-art/**/*.njk");
-  });
-
-  // Deutsche Games-Seiten
-  eleventyConfig.addCollection("gamesDE", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("src/pages/games/**/*.njk");
-  });
-
-  // Englische Games-Seiten
-  eleventyConfig.addCollection("gamesEN", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("src/pages/en/games/**/*.njk");
-  });
-
   return {
     dir: {
       input: "src",
